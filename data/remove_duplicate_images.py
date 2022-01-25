e2452808646b1684337c31b5d5f95bcd0f88f124
shyamalanadkat <sanadkat@grubhub.com>
2022-01-24 19:36:57 -0500

    push updates after demo

--- data/remove_duplicate_images.py
+++ data/remove_duplicate_images.py
@@ -8,3 +8,6 @@ dif("raw/russian", delete=True, similarity="high")
 
 print('Deleting duplicate telugu images...')
 dif("raw/telugu", delete=True, similarity="high")
+
+print('Deleting duplicate english images...')
+dif("raw/english", delete=True, similarity="high")
