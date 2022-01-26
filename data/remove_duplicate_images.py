4eebdece291a1a630af3ae0a488e7fcea03e63e5
shyamalanadkat <sanadkat@grubhub.com>
2022-01-26 13:04:40 -0500

    Revert "Uploaded the unedited (grayscale/sizeing) of english text"
    This reverts commit 1d754a55158c6a556d710caa03b4b4f4fb095a7b.

---
+++ data/remove_duplicate_images.py
@@ -0,0 +1,10 @@
+from difPy import dif
+
+print('Deleting duplicate russian images...')
+dif("raw/russian", delete=True, similarity="high")
+
+print('Deleting duplicate telugu images...')
+dif("raw/telugu", delete=True, similarity="high")
+
+print('Deleting duplicate english images...')
+dif("raw/english", delete=True, similarity="high")
