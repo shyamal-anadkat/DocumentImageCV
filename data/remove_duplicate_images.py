eb4369cab3681c7630c7eccb4c82d9a25f6cbb9b
shyamalanadkat <sanadkat@grubhub.com>
2022-01-26 07:29:31 -0500

    add more telugu raw data

--- data/remove_duplicate_images.py
+++ data/remove_duplicate_images.py
@@ -1,8 +1,5 @@
 from difPy import dif
 
-print('Deleting duplicate gujarati images...')
-dif("raw/gujarati", delete=True, similarity="high")
-
 print('Deleting duplicate russian images...')
 dif("raw/russian", delete=True, similarity="high")
 
