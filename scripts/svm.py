421a2a4e3af1969d6505ae0ebacb29dac6ef513a
1999Lyd <87921304+1999Lyd@users.noreply.github.com>
2022-02-02 08:33:11 +0800

    Update svm.py

--- scripts/svm.py
+++ scripts/svm.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import accuracy_score,plot_confusion_matrix
-import pickle
+
 from thundersvm import SVC
 from make_dataset_svm import transform_data
 train_dataset_array, train_dataset_label, val_dataset_array, val_dataset_label, images, dataloaders, batch_size, class_names, dataset_sizes = transform_data()
