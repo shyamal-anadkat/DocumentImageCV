82b768cafbbe4d69f4d6e1c98208956ed2954666
shyamalanadkat <sanadkat@grubhub.com>
2022-02-03 15:16:30 -0500

    update to 20 epochs

--- scripts/main.py
+++ scripts/main.py
@@ -3,5 +3,5 @@ from model import train_model
 
 images, dataloaders, batch_size, class_names, dataset_sizes = transform_data()
 model = train_model(
-    images, dataloaders, batch_size, class_names, dataset_sizes, num_epochs=10
+    images, dataloaders, batch_size, class_names, dataset_sizes, num_epochs=20
 )
