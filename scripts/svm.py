c86977d2eda653dfb7392a382f3b4ecd42d116a6
Liu Yudong <87921304+1999Lyd@users.noreply.github.com>
2022-02-07 08:21:36 +0800

    Update svm.py

--- scripts/svm.py
+++ scripts/svm.py
@@ -1,8 +1,9 @@
 from sklearn.metrics import accuracy_score, plot_confusion_matrix
-
+import matplotlab.pyplot as plt
 from thundersvm import SVC
 from make_dataset_svm import transform_data
 
+#get dataset for training
 (
     train_dataset_array,
     train_dataset_label,
@@ -18,8 +19,15 @@ x_train = train_dataset_array
 y_train = train_dataset_label
 x_val = val_dataset_array
 y_val = val_dataset_label
+
+#define model
 model = SVC(C=100, kernel="rbf")
+
+#train model
 model.fit(x_train, y_train)
+
+#predict output with validation dataset
 y_pred = model.predict(x_val)
 print(f"The model is {accuracy_score(y_pred,y_val)*100}% accurate")
 plot_confusion_matrix(model, x_val, y_val)
+plt.show()
