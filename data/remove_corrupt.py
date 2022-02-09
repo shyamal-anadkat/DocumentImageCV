d46293f3392de6c0e835426ff61bda3fcf528141
Chris <christopher.oblak@duke.edu>
2022-02-09 11:11:23 -0500

    minor changed to doc strings

--- data/remove_corrupt.py
+++ data/remove_corrupt.py
@@ -7,6 +7,9 @@ def remove_corrupt_and_nonjpg_images(dir_path):
     """
      Removes corrupt & non jpg images from the specified dir path for raw data
     :param dir_path: path to directory with raw images
+    
+    Use: run with in "data" foldler 
+    cmd line -> python remove_corrupt.py
     """
     cntr = 0
     for filename in listdir(dir_path):
--- data/remove_duplicate.py
+++ data/remove_duplicate.py
@@ -5,7 +5,7 @@ def remove_duplicate_images(dir_path):
     """
     Removes duplicate and low resolution raw data images within the specified directory
     similarity: "high" = search for exact duplicates, very sensitive to details
-    :param dir_path:
+    :param dir_path: python remove_duplicate.py
     """
     print(f"--> Deleting duplicate images within {dir_path}")
     dif(dir_path, delete=True, similarity="high")
